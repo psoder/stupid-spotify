83495befe7348ed4520d507d349b7899d9b4c31b
Pontus <52171526+psoder@users.noreply.github.com>
2023-03-31 13:23:39 +0200

    Don't create client when user is not signed in

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                transparent: "transparent",
+                current: "currentColor",
+                primary: "#1DB954",
+                black: "#191414"
+            }
+        }
     },
     plugins: []
 };
