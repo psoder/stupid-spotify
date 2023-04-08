967a402cc13c66239a28c63c0ec2bd227d9a2da8
Oskar Bärnstörm <78731481+Barnadottir@users.noreply.github.com>
2023-04-09 00:24:13 +0200

    Create Playlist.tsx
    Adding some functionality to be able to display name of current playlists, will add more later.

---
+++ src/components/Playlist.tsx
@@ -0,0 +1,50 @@
+import {useState,useEffect} from "react";
+import type { SpotifyClient, PlaylistSimplified, playlists  } from "spotify-api.js";
+import getUserPlaylists from "spotify-api.js";
+import errorHandler from "@/util/ErrorHandler";
+import { execPath } from "process";
+import { PlaylistManager } from "spotify-api.js";
+import PlaylistHelper from "./PlaylistHelper";
+
+interface Props{
+    spotifyClient: SpotifyClient
+}
+
+const Playlist = ({spotifyClient}: Props) => {//unfinished
+    console.log("här har vi den")
+    //perform initial search for playlists:
+    const [playlistData,setPlaylistData] = useState<{ playlist?: PlaylistSimplified[] }>({});
+    const [playlistArray,setPlaylistArray] = useState<string[]>();
+    async function fetchUserPlaylists(spotifyClient: SpotifyClient) {
+            try {
+                const userPlaylists = spotifyClient?.fetch('/me/playlists');//get user playlists
+                userPlaylists.then( async (playlists) => { 
+                    const userPlaylists = playlists.items;
+                    const fetchedPlaylistArray =  await PlaylistHelper({spotifyClient,userPlaylists});
+                    setPlaylistArray(fetchedPlaylistArray);
+                }).catch((e) => {
+                    console.log(e);
+                });
+            }
+        catch(e){
+            console.log(e)
+            //errorHandler(e);
+        }
+    };
+    if (!playlistArray) fetchUserPlaylists(spotifyClient);
+    useEffect(() => {
+        fetchUserPlaylists(spotifyClient);
+      }, [spotifyClient]);
+    return(
+        <div>
+            <ul> Current Playlists:
+                {playlistArray ? playlistArray.map((playlist,idx) => {
+                   return <li key = {idx}>{playlist}</li>
+                }): null}
+            </ul>
+        </div>
+    );
+}
+
+export default Playlist;
+
