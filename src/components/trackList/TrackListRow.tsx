ad187afa705c7833fa326c5f305f6b040a0b69ce
Pontus Söderlund <52171526+psoder@users.noreply.github.com>
2023-04-10 18:36:46 +0200

    Improve error messages (#28)
    Part of #10.

--- src/components/trackList/TrackListRow.tsx
+++ src/components/trackList/TrackListRow.tsx
@@ -1,5 +1,6 @@
 import { formatTime } from "@/util";
 import { getSession } from "next-auth/react";
+import { toast } from "react-toastify";
 import type { Track as TrackType } from "spotify-api.js";
 import Track from "../Track";
 
@@ -7,12 +8,17 @@ const TrackListRow = ({ track }: { track: TrackType }) => {
     const handleClick = async () => {
         const accessToken = (await getSession())?.accessToken;
 
-        fetch(`https://api.spotify.com/v1/me/player/queue?uri=${track.uri}`, {
+        const p = fetch(`https://api.spotify.com/v1/me/player/queue?uri=${track.uri}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${accessToken}`
             }
+        });
+
+        toast.promise(p, {
+            success: `Added "${track.name}" to queue.`,
+            error: `Could not add "${track.name}" to queue.`
         });
     };
 
--- src/modules/player/PlayerContent.tsx
+++ src/modules/player/PlayerContent.tsx
@@ -1,12 +1,8 @@
 import Track from "@/components/Track";
 import { getSession } from "next-auth/react";
 import { useEffect } from "react";
-import {
-    useErrorState,
-    usePlaybackState,
-    usePlayerDevice,
-    useWebPlaybackSDKReady
-} from "react-spotify-web-playback-sdk";
+import { useErrorState, usePlaybackState, usePlayerDevice } from "react-spotify-web-playback-sdk";
+import { toast } from "react-toastify";
 import PlayerControls from "./PlayerControls";
 import PlayerProgessBar from "./PlayerProgressBar";
 import PlayerVolumeControl from "./PlayerVolumeControl";
@@ -15,36 +11,40 @@ const PlayerContent = () => {
     const playbackState = usePlaybackState(true, 500);
     const playerDevice = usePlayerDevice();
     const errorState = useErrorState();
-    const webPlaybackSDKReady = useWebPlaybackSDKReady();
 
     useEffect(() => {
-        if (playerDevice?.device_id === undefined) return;
+        if (playerDevice?.status !== "ready") {
+            return;
+        }
 
         const transferPlayback = async () => {
             const accessToken = (await getSession())?.accessToken;
 
-            // https://developer.spotify.com/documentation/web-api/reference/#endpoint-transfer-a-users-playback
-            await fetch(`https://api.spotify.com/v1/me/player`, {
+            // https://developer.spotify.com/documentation/web-api/reference/transfer-a-users-playback
+            const p = fetch(`https://api.spotify.com/v1/me/player`, {
                 method: "PUT",
-                body: JSON.stringify({ device_ids: [playerDevice.device_id], play: false }),
+                body: JSON.stringify({ device_ids: [playerDevice.device_id], play: true }),
                 headers: {
                     "Content-Type": "application/json",
                     Authorization: `Bearer ${accessToken}`
                 }
             });
 
-            console.log("Transfared playback");
+            toast.promise(p, {
+                pending: "Transfering playback.",
+                success: "Successfully transfered playback!",
+                error: "Something went wrong."
+            });
         };
 
         transferPlayback();
-    }, [playerDevice?.device_id]);
+    }, [playerDevice]);
 
     useEffect(() => {
-        console.log("webPlaybackSDKReady", webPlaybackSDKReady);
-    }, [webPlaybackSDKReady]);
-
-    useEffect(() => {
-        console.error("errorState", errorState);
+        if (errorState) {
+            toast.error("Something went wrong. See the console for more information.");
+            console.error(errorState);
+        }
     }, [errorState]);
 
     return (
