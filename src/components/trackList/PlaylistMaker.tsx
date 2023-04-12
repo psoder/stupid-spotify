396d31ccbbf8247a54e8f6aba28a09e43a67a052
Oskar <baernstroem.oskar@gmail.com>
2023-04-12 15:31:40 +0200

    baisc playlist implementation

---
+++ src/components/trackList/PlaylistMaker.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import type { SpotifyClient, Playlist } from "spotify-api.js";
+import PlaylistPrinter from "./playlistPrinter";
+
+interface Props {
+    spotifyClient: SpotifyClient;
+}
+
+const PlaylistMaker = ({ spotifyClient }: Props) => {
+    //unfinished
+    //perform initial search for playlists:
+    const [playlistArray, setPlaylistArray] = useState<Playlist[]>();
+    async function fetchUserPlaylists(spotifyClient: SpotifyClient) {
+        try {
+            const userPlaylists: Promise<Playlist> = spotifyClient?.fetch("/me/playlists"); //get user playlists
+            userPlaylists
+                .then(async (playlists) => {
+                    const userPlaylists = playlists.items;
+                    //console.log(userPlaylists[0].name);
+                    setPlaylistArray(userPlaylists);
+                    //const fetchedPlaylistArray =  await PlaylistHelper({spotifyClient,userPlaylists});
+                    //setPlaylistArray(fetchedPlaylistArray);
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        } catch (e) {
+            console.log(e);
+            //errorHandler(e);
+        }
+    }
+    if (!playlistArray) fetchUserPlaylists(spotifyClient);
+    useEffect(() => {
+        fetchUserPlaylists(spotifyClient);
+    }, [spotifyClient]);
+    return <div>{playlistArray ? <PlaylistPrinter playlists={playlistArray} /> : null}</div>;
+};
+
+export default PlaylistMaker;
