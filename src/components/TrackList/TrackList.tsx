2bcab373a3ecb0bcc1eafbe7b0d87250c082d6d7
Pontus Söderlund <52171526+psoder@users.noreply.github.com>
2023-05-07 15:30:37 +0200

    Update list styling (#51)

--- src/components/TrackList/TrackList.tsx
+++ src/components/TrackList/TrackList.tsx
@@ -6,7 +6,7 @@ export const TrackList = ({ tracks }: { tracks: Track[] }) => {
     if (tracks.length > 0) {
         return (
             <table className="grid gap-2">
-                <thead className="track-list-grid-columns card items-center font-bold">
+                <thead className="track-list-grid-columns card">
                     <td className="col-span-6">Title</td>
                     <td className="col-span-4">Album</td>
                     <td className="col-span-1 ml-auto">
