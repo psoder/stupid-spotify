6778f2ed85b4d24d9afd0d615672dd150439836d
xiaoming-bao <125286062+xiaoming-bao@users.noreply.github.com>
2023-05-08 17:13:30 +0200

    Update src/pages/api/luckywords.ts
    Co-authored-by: Pontus Söderlund <52171526+psoder@users.noreply.github.com>

--- src/pages/api/luckywords.ts
+++ src/pages/api/luckywords.ts
@@ -1,5 +1,5 @@
 import fs from "fs/promises";
-import { getrandominteger } from "@/util";
+import { getRandomInteger } from "@/util";
 
 export default async function handler(req, res) {
     let wordlist: string[] = [];
