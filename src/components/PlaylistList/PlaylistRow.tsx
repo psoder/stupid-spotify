befe4d925b2e53e64d3424b99656b59d66e2b940
Pontus <52171526+psoder@users.noreply.github.com>
2023-05-10 15:59:20 +0200

    Remove duplicate CSS

--- src/components/PlaylistList/PlaylistRow.tsx
+++ src/components/PlaylistList/PlaylistRow.tsx
@@ -30,7 +30,7 @@ export const PlaylistRow = ({ playlist }: { playlist: Playlist }) => {
                 onClose={() => setShowPlaylistContent(false)}
                 className="items-center justify-center"
             >
-                <div className="card relative h-[900px] w-[1000px] bg-black-heavy p-16">
+                <div className="card h-[900px] w-[1000px] bg-black-heavy p-16">
                     <div className="relative h-full">
                         <button
                             className="absolute right-0 hover:text-primary"
