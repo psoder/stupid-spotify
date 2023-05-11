073ed7464a80eebb2411da2ae849a1de489afd84
Pontus <52171526+psoder@users.noreply.github.com>
2023-05-11 11:06:18 +0200

    Continue work on context menu

--- src/components/TrackList/TrackListContextMenu.tsx
+++ src/components/TrackList/TrackListContextMenu.tsx
@@ -1,28 +1,43 @@
 import { useContextMenu } from "@/hooks/useContextMenu";
-import { ReactNode } from "react";
+import { createElement, ReactNode } from "react";
 
-export const TrackListContextMenu = ({ children }: { children: ReactNode }) => {
+export const TrackListContextMenu = ({
+    actions,
+    children,
+    renderAs = "div",
+    className
+}: {
+    actions: { name: string; onClick: (arg: any) => void }[];
+    children: ReactNode;
+    renderAs?: string;
+    className?: string;
+}) => {
     const { position, setPosition, active, setActive } = useContextMenu();
 
     return (
-        <div className="overflow-auto">
-            <div
-                onContextMenu={(e) => {
-                    e.preventDefault();
-                    setActive(true);
-                    setPosition({ x: e.pageX, y: e.pageY });
-                }}
-            >
-                {children}
-            </div>
+        <>
+            {createElement(
+                renderAs,
+                {
+                    className: className,
+                    onContextMenu: (e) => {
+                        e.preventDefault();
+                        setActive(true);
+                        setPosition({ x: e.pageX, y: e.pageY });
+                    }
+                },
+                children
+            )}
 
             {active && (
                 <ul style={{ top: position.y, left: position.x }} className="card fixed p-0">
-                    <li>Add to queue</li>
-                    <li>Add to playlist</li>
-                    <li>Remove from playlist</li>
+                    {actions.map((action) => (
+                        <li key={Math.random()} onClick={action.onClick}>
+                            {action.name}
+                        </li>
+                    ))}
                 </ul>
             )}
-        </div>
+        </>
     );
 };
--- src/hooks/useContextMenu.ts
+++ src/hooks/useContextMenu.ts
@@ -5,14 +5,9 @@ export const useContextMenu = () => {
     const [position, setPosition] = useState({ x: 0, y: 0 });
 
     useEffect(() => {
-        const handleClick = (e) => {
-            setActive(false);
-            console.log(e);
-        };
+        const handleClick = () => setActive(false);
 
         document.addEventListener("click", handleClick);
-        console.log("hepp");
-        
 
         return () => {
             document.removeEventListener("click", handleClick);
