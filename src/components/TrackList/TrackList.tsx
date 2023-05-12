474e20f24b9d64f57695edf8b4dc44a6f6141407
Pontus <52171526+psoder@users.noreply.github.com>
2023-05-12 13:41:58 +0200

    Update TrackList element type

--- src/components/TrackList/TrackList.tsx
+++ src/components/TrackList/TrackList.tsx
@@ -5,23 +5,21 @@ import { TrackListRow } from "./TrackListRow";
 export const TrackList = ({ tracks }: { tracks: Track[] }) => {
     if (tracks.length > 0) {
         return (
-            <table className="relative flex h-full flex-col overflow-hidden">
-                <thead>
-                    <tr className="track-list-grid-columns card border-b border-b-black-lightest p-3">
-                        <td className="col-span-6">Title</td>
-                        <td className="col-span-4">Album</td>
-                        <td className="col-span-1 ml-auto">
-                            <TbClockPlay className="text-gray-lightest" size={24} />
-                        </td>
-                        <td />
-                    </tr>
-                </thead>
-                <tbody className="overflow-scroll">
+            <div className="relative flex h-full flex-col overflow-hidden">
+                <div className="track-list-grid-columns card border-b border-b-black-lightest p-3">
+                    <div className="col-span-6">Title</div>
+                    <div className="col-span-4">Album</div>
+                    <div className="col-span-1 ml-auto">
+                        <TbClockPlay className="text-gray-lightest" size={24} />
+                    </div>
+                    <div />
+                </div>
+                <div className="overflow-scroll">
                     {tracks?.map((track) => (
                         <TrackListRow key={track.id} track={track} />
                     ))}
-                </tbody>
-            </table>
+                </div>
+            </div>
         );
     } else {
         return <>No tracks</>;
