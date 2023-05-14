29664fe7b16b5b045cd234ccd0276f9988875864
xiaoming-bao <125286062+xiaoming-bao@users.noreply.github.com>
2023-05-14 18:30:28 +0200

    Revert "Welcome page (#49)"
    This reverts commit e5a6a5c7096d74a24f36614f7e94cdb345d2a334.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,16 +9,13 @@ import { Roboto_Flex } from "next/font/google";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useRouter } from "next/router";
 
 const font = Roboto_Flex({
     subsets: ["latin"]
 });
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-    const router = useRouter();
-
-    return router.pathname == "/search" ? (
+    return (
         <>
             <Head>
                 <title>Stupid Spotify</title>
@@ -37,29 +34,6 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
                             <Player />
                         </SpotifyPlaybackProvider>
                     </SpotifyUserClientProvider>
-                </SessionProvider>
-            </div>
-
-            <ToastContainer position="bottom-right" autoClose={3000} theme="dark" />
-        </>
-    ) : (
-        <>
-            <Head>
-                <title>Stupid Spotify</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/doughnut-cat.png" />
-            </Head>
-
-            <div className={`flex min-h-screen w-full flex-col bg-primary ${font.className}`}>
-                <SessionProvider session={session}>
-                    <Header />
-                    <SpotifyClientProvider>
-                        <SpotifyPlaybackProvider>
-                            <div className="flex-grow">
-                                <Component {...pageProps} />
-                            </div>
-                        </SpotifyPlaybackProvider>
-                    </SpotifyClientProvider>
                 </SessionProvider>
             </div>
 
